db831a0ff277bc9bd8c77bef649492e47a9a180f
Kevin Tran <30328540+kevtran23@users.noreply.github.com>
2019-12-12 02:18:37 -0500

    Update data.py

--- data.py
+++ data.py
@@ -22,6 +22,50 @@ class CaptionedImageDataset(Dataset):
     def __len__(self) -> int:
         raise NotImplementedError
 
+class CocoDataset(data.Dataset):
+    """COCO Custom Dataset compatible with torch.utils.data.DataLoader."""
+    def __init__(self, root, json, vocab, transform=None):
+        """Set the path for images, captions and vocabulary wrapper.
+        
+        Args:
+            root: image directory.
+            json: coco annotation file path.
+            vocab: vocabulary wrapper.
+            transform: image transformer.
+        """
+        self.root = root
+        self.coco = COCO(json)
+        self.ids = list(self.coco.anns.keys())
+        self.vocab = vocab
+        self.transform = transform
+
+    def __getitem__(self, index):
+        """Returns one data pair (image and caption)."""
+        coco = self.coco
+        vocab = self.vocab
+        ann_id = self.ids[index]
+        caption = coco.anns[ann_id]['caption']
+        img_id = coco.anns[ann_id]['image_id']
+        path = coco.loadImgs(img_id)[0]['file_name']
+
+        image = Image.open(os.path.join(self.root, path)).convert('RGB')
+        if self.transform is not None:
+            image = self.transform(image)
+
+        # Convert caption (string) to word ids.
+        tokens = nltk.tokenize.word_tokenize(str(caption).lower())
+        caption = []
+        caption.append(vocab('<start>'))
+        caption.extend([vocab(token) for token in tokens])
+        caption.append(vocab('<end>'))
+        target = torch.Tensor(caption)
+        return image, target
+
+    def __len__(self):
+        return len(self.ids)
+
+
+
 
 class Imagenet32Dataset(CaptionedImageDataset):
     def __init__(self, root="datasets/ImageNet32", train=True, max_size=-1):
@@ -82,6 +126,8 @@ class CIFAR10Dataset(CaptionedImageDataset):
         img = 2 * img - 1
         text_label = self.text_labels[label]
         return img, label, text_label
+
+
 
 
 def load_vocab_imagenet(vocab_file):
